0fadf2ec8daf30e7b9a898b0bc72d5a844ea90cd
Utsuda Shinou <54788151+utsudashinou@users.noreply.github.com>
2020-08-26 20:04:36 +0900

    1.1.3 2020/08/26 Fix bug that breath of pictures in map stops after battle

--- UTSU_PictureBreath.js
+++ UTSU_PictureBreath.js
@@ -6,6 +6,7 @@
 // http://opensource.org/licenses/mit-license.php
 // ----------------------------------------------------------------------------
 // Version
+// 1.1.3 2020/08/26 Fix bug that breath of pictures in map stops after battle
 // 1.1.2 2020/08/25 Fix to continue picture breath even if picture changed for the same picture number
 // 1.1.1 2020/08/25 Fix bug about init params
 // 1.1.0 2020/08/23 Fix plugin does not work in a battle
@@ -51,12 +52,19 @@
   const STATE_REQUEST_DEACTIVATE = 0;
   const STATE_REQUEST_ACTIVATE = 1;
 
+  const breathBackup = {};
+
   global.UTSU.PictureBreath.on = (pids, period) => {
     pids.forEach((pid) => {
       const picture = $gameScreen.picture(Number(pid));
       if (picture) {
         picture._breathState = STATE_REQUEST_ACTIVATE;
         picture._breathPeriod = Number(period);
+
+        const realPictureId = $gameScreen.realPictureId(Number(pid));
+        breathBackup[realPictureId] = {
+          _breathPeriod: picture._breathPeriod,
+        };
       }
     });
   };
@@ -66,6 +74,9 @@
       const picture = $gameScreen.picture(Number(pid));
       if (picture) {
         picture._breathState = STATE_REQUEST_DEACTIVATE;
+
+        const realPictureId = $gameScreen.realPictureId(Number(pid));
+        breathBackup[realPictureId] = null;
       }
     });
   };
@@ -82,47 +93,58 @@
     }
   };
 
+  const _Game_Screen_showPicture = Game_Screen.prototype.showPicture;
+  Game_Screen.prototype.showPicture = function (pictureId, name, origin, x, y, scaleX, scaleY, opacity, blendMode) {
+    _Game_Screen_showPicture.call(this, pictureId, name, origin, x, y, scaleX, scaleY, opacity, blendMode);
+    const realPictureId = this.realPictureId(pictureId);
+    if (breathBackup[realPictureId]) {
+      const picture = this._pictures[realPictureId];
+      picture._breathState = STATE_REQUEST_ACTIVATE;
+      picture._breathPeriod = breathBackup[realPictureId]._breathPeriod;
+    }
+  };
+
   const _Game_Picture_initBasic = Game_Picture.prototype.initBasic;
   Game_Picture.prototype.initBasic = function () {
     _Game_Picture_initBasic.call(this);
+    this._breathActive = false;
     this._breathState = STATE_NO_OPERATION;
     this._breathPeriod = 0;
+    this._breathCount = 0;
+  };
+
+  const _Game_Picture_update = Game_Picture.prototype.update;
+  Game_Picture.prototype.update = function () {
+    _Game_Picture_update.call(this);
+    if (this._breathState === STATE_REQUEST_ACTIVATE) {
+      this._breathState = STATE_NO_OPERATION;
+      this._breathActive = true;
+      this._breathCount = 0;
+    }
+    if (this._breathState === STATE_REQUEST_DEACTIVATE) {
+      this._breathState = STATE_NO_OPERATION;
+      this._breathActive = false;
+    }
+    if (this._breathActive) {
+      this._breathCount = (this._breathCount + 1) % this._breathPeriod;
+    }
   };
 
   const _Sprite_Picture_update = Sprite_Picture.prototype.update;
   Sprite_Picture.prototype.update = function () {
     _Sprite_Picture_update.call(this);
     if (this.visible) {
-      this._breathCheck();
       this._breathUpdate();
     }
   };
 
-  Sprite_Picture.prototype._breathCheck = function () {
+  Sprite_Picture.prototype._breathUpdate = function () {
     const picture = this.picture();
     if (!picture) {
       return;
     }
-    // if (this._breathPeriod === picture._breathPeriod) {
-    //   picture._breathState = STATE_REQUEST_ACTIVATE;
-    // }
-    if (picture._breathState === STATE_REQUEST_ACTIVATE) {
-      picture._breathState = STATE_NO_OPERATION;
-      this._breathActive = true;
-      this._breathPeriod = picture._breathPeriod;
-      this._breathCount = 0;
-    }
-    if (picture._breathState === STATE_REQUEST_DEACTIVATE) {
-      picture._breathState = STATE_NO_OPERATION;
-      this._breathActive = false;
-      this._breathPeriod = 0;
-    }
-  };
-
-  Sprite_Picture.prototype._breathUpdate = function () {
-    if (this._breathActive) {
-      this._breathCount = (this._breathCount + 1) % this._breathPeriod;
-      const freq = Math.sin((Math.PI * this._breathCount) / (this._breathPeriod / 2));
+    if (picture._breathActive) {
+      const freq = Math.sin((Math.PI * picture._breathCount) / (picture._breathPeriod / 2));
       this.scale.y -= freq * 0.015 + 0.015;
       this.y -= Math.ceil((this.height * (1.0 - this.scale.y)) / 2);
       this.scale.x += freq * 0.005 + 0.005;
